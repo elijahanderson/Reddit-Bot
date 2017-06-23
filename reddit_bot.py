45a9c56444c25306bbabe91407cb8ffa04943526
elijahanderson <elijahanderson_2019@depauw.edu>
2017-06-23 10:38:02 -0400

    Deleted praw.ini, added run_bot() function
    Added actual bot function.
    
    Deleted praw.ini because it was giving me trouble, and it's just easier not to bother.

--- reddit_bot.py
+++ reddit_bot.py
@@ -3,4 +3,28 @@ import praw
 
 # Login the bot into reddit
 
-praw.Reddit('yeah_bot_test', user_agent = 'yeah_bot_test v0.1')
+
+def bot_login() :
+    print('Logging in...')
+    reddit = praw.Reddit(client_id='gZGNheQ3FFswZQ',
+                         client_secret='whHLVVFyZJ3VJ5K7joNmCTHuVd4',
+                         password='elephantsanddonkeysgrowbigears',
+                         user_agent = 'yeah_bot_test v0.1',
+                         username = 'yeah_bot_test')
+    print(reddit.user.me())
+    return reddit
+
+
+def run_bot(reddit) :
+
+    # Loop through the top 25 comments in all posts certain subreddit
+    for comment in reddit.subreddit('test').comments(limit=25) :
+
+        #check if keyword '!showyeahs' is in any of those comments
+
+        if '!showyeahs' in comment.body :
+            print('String found!!')         #Placeholder for code that will be implemented at later time
+
+
+reddit = bot_login()
+run_bot(reddit)
