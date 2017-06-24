873a8d25d723b9c106a3548e93d66c920c4060a0
elijahanderson <elijahanderson_2019@depauw.edu>
2017-06-24 15:25:44 -0400

    Prevent spam
    Added list comments_replied_to to keep track of comments replied to so the bot won't spam

--- reddit_bot.py
+++ reddit_bot.py
@@ -21,20 +21,25 @@ def bot_login() :
     return reddit
 
 
-def run_bot(reddit) :
+def run_bot(reddit, comments_replied_to) :
 
     # Loop through the top 25 comments in all posts on a certain subreddit
     for comment in reddit.subreddit('test').comments(limit=25) :
 
-        #check if keyword '!showyeahs' is in any of those comments
+        #check if keyword '!showyeahs' is in any of those comments and comment has already been replied to
 
-        if '!showyeahs' in comment.body :
+        if '!showyeahs' in comment.body and comment.id not in comments_replied_to:
             print('String found!!')         #Placeholder for code that will be implemented at later time
 
             #reply via comment
 
             print('Replied to ' + comment.id)
+
             comment.reply('String found!! [Yeah!](https://byyeah.com/assets/img/product/outofstock.jpg)')
+
+            # Add comment ID to replied to list
+            comments_replied_to.append(comment.id)
+
     # Sleep for ten seconds
 
     print('Sleeping for 10 seconds...')
@@ -42,7 +47,11 @@ def run_bot(reddit) :
 
 reddit = bot_login()
 
+# To prevent spam, create list of comments already replied to
+
+comments_replied_to = []
+
 # To automatically reply to comments, a while loop is used
 
 while True :
-    run_bot(reddit)
+    run_bot(reddit, comments_replied_to)
