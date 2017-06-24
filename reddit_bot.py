72c7d8dff07583f70fc405e0bbbb6343abf468c3
elijahanderson <elijahanderson_2019@depauw.edu>
2017-06-24 14:33:08 -0400

    Changed account that bot was registered to
    Changed username, password, client_id, and client_secret accordingly

--- reddit_bot.py
+++ reddit_bot.py
@@ -1,4 +1,6 @@
 # praw is a tool that makes interacting with reddit much easier.
+#
+
 import praw
 
 # Login the bot into reddit
@@ -6,11 +8,11 @@ import praw
 
 def bot_login() :
     print('Logging in...')
-    reddit = praw.Reddit(client_id='gZGNheQ3FFswZQ',
-                         client_secret='whHLVVFyZJ3VJ5K7joNmCTHuVd4',
-                         password='a147258b',
+    reddit = praw.Reddit(client_id='kQ-LllZafD8X5Q',
+                         client_secret='wB-SNo-_ImXjPIVCKaFjbobP_Zg',
+                         password='elephantsanddonkeysgrowbigears',
                          user_agent='yeah_bot_test v0.1',
-                         username='moldyxorange')
+                         username='yeah_bot_test')
     print('Logged in as ' + str(reddit.user.me()))
     return reddit
 
