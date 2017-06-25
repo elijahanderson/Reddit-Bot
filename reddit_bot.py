cc0e340f1821d7428fc848876b1da758e703594b
elijahanderson <elijahanderson_2019@depauw.edu>
2017-06-25 10:45:50 -0400

    Added get_saved_comments()
    - program now saves comments that have been replied to to .txt file

--- reddit_bot.py
+++ reddit_bot.py
@@ -6,17 +6,18 @@ import praw
 # time module is used so the bot won't go crazy
 
 import time
+import os
 
 # Login the bot into reddit
 
 
 def bot_login() :
     print('Logging in...')
-    reddit = praw.Reddit(client_id='kQ-LllZafD8X5Q',
-                         client_secret='wB-SNo-_ImXjPIVCKaFjbobP_Zg',
-                         password='elephantsanddonkeysgrowbigears',
+    reddit = praw.Reddit(client_id='gZGNheQ3FFswZQ',
+                         client_secret='whHLVVFyZJ3VJ5K7joNmCTHuVd4',
+                         password='a147258b',
                          user_agent='yeah_bot_test v0.1',
-                         username='yeah_bot_test')
+                         username='moldyxorange')
     print('Logged in as ' + str(reddit.user.me()))
     return reddit
 
@@ -26,12 +27,13 @@ def run_bot(reddit, comments_replied_to) :
     # Loop through the top 25 comments in all posts on a certain subreddit
     for comment in reddit.subreddit('test').comments(limit=25) :
 
-        #check if keyword '!showyeahs' is in any of those comments and comment has already been replied to
+        # check if keyword '!showyeahs' is in any of those comments and comment has already been replied to
 
         if '!showyeahs' in comment.body and comment.id not in comments_replied_to:
-            print('String found!!')         #Placeholder for code that will be implemented at later time
 
-            #reply via comment
+            print('String found!!')
+
+            # reply via comment
 
             comment.reply('String found!! [Yeah!](https://byyeah.com/assets/img/product/outofstock.jpg)')
             print('Replied to ' + comment.id)
@@ -39,22 +41,43 @@ def run_bot(reddit, comments_replied_to) :
             # Add comment ID to replied to list
             comments_replied_to.append(comment.id)
 
-            # Sleep for ten seconds
+            # Save comment ID to comments_replied_to.txt (the 'a' means i am appending to the file)
 
-            print('Sleeping for 10 seconds...')
-            time.sleep(10)
+            with open('comments_replied_to.txt', 'a') as file:
+                file.write(comment.id + '\n')
 
     # Sleep for ten seconds
 
     print('Sleeping for 10 seconds...')
     time.sleep(10)
 
+# Save the comments that have been replied to in the past so the bot doesn't reply to same comments the after each time
+# it is run
+#
+# Uses .txt file to store the comments
+
+def get_saved_comments() :
+
+    if not os.path.isfile('comments_replied_to.txt') :
+        comments_replied_to = []
+
+    else :
+        with open('comments_replied_to.txt', 'r') as file :
+
+            # Read contents of the file
+            comments_replied_to = file.read()
+
+            # split() by new line
+            comments_replied_to = comments_replied_to.split('\n')
+
+    return comments_replied_to
+
 reddit = bot_login()
 
 # To prevent spam, create list of comments already replied to
 
-comments_replied_to = []
-
+comments_replied_to = get_saved_comments()
+print(comments_replied_to)
 # To automatically reply to comments, a while loop is used
 
 while True :
