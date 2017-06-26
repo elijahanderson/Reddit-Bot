2eb66bd831cdb6138238c190a1cac23bf6040af3
elijahanderson <elijahanderson_2019@depauw.edu>
2017-06-26 10:48:25 -0400

    Syntax Changes
    - changed bot_login() to authenticate()
    - implemented praw.ini for additional security

--- reddit_bot.py
+++ reddit_bot.py
@@ -8,30 +8,28 @@ import praw
 import time
 import os
 
+
 # Login the bot into reddit
 
 
-def bot_login() :
-    print('Logging in...')
-    reddit = praw.Reddit(client_id='gZGNheQ3FFswZQ',
-                         client_secret='whHLVVFyZJ3VJ5K7joNmCTHuVd4',
-                         password='a147258b',
-                         user_agent='yeah_bot_test v0.1',
-                         username='moldyxorange')
-    print('Logged in as ' + str(reddit.user.me()))
+def authenticate() :
+    print('Authenticating...')
+    reddit = praw.Reddit('YeahBot', user_agent='yeah_bot_test v0.1')
+    print('Authenticated as ' + str(reddit.user.me()))
     return reddit
 
 
 def run_bot(reddit, comments_replied_to) :
 
     # Loop through the top 25 comments in all posts on a certain subreddit
+
     for comment in reddit.subreddit('test').comments(limit=25) :
 
         # check if keyword '!showyeahs' is in any of those comments and comment has already been replied to
 
         if '!showyeahs' in comment.body and comment.id not in comments_replied_to:
 
-            print('String found!!')
+            print('String with \'!showyeahs\' found!!')
 
             # reply via comment
 
@@ -41,7 +39,7 @@ def run_bot(reddit, comments_replied_to) :
             # Add comment ID to replied to list
             comments_replied_to.append(comment.id)
 
-            # Save comment ID to comments_replied_to.txt (the 'a' means i am appending to the file)
+            # Save comment ID to comments_replied_to.txt (the 'a' means I am appending to the file)
 
             with open('comments_replied_to.txt', 'a') as file:
                 file.write(comment.id + '\n')
@@ -56,7 +54,10 @@ def run_bot(reddit, comments_replied_to) :
 #
 # Uses .txt file to store the comments
 
+
 def get_saved_comments() :
+
+    # If .txt file with comment IDs doesnt exist, create one and return a blank array
 
     if not os.path.isfile('comments_replied_to.txt') :
         comments_replied_to = []
@@ -70,14 +71,19 @@ def get_saved_comments() :
             # split() by new line
             comments_replied_to = comments_replied_to.split('\n')
 
+            # Filter out the empty string at end of the .txt file
+            # filter() filters out the first argument from the second argument
+            # comments_replied_to = filter('', comments_replied_to)
+
     return comments_replied_to
 
-reddit = bot_login()
+reddit = authenticate()
 
 # To prevent spam, create list of comments already replied to
 
 comments_replied_to = get_saved_comments()
 print(comments_replied_to)
+
 # To automatically reply to comments, a while loop is used
 
 while True :
